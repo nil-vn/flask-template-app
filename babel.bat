e4a5fd65ee8ed84086d651d4e2ea317a2627b5b6
TinhTN <tinhtn.hrs@gmail.com>
2025-10-04 01:02:07 +0900

    feat: v1

--- babel.bat
+++ babel.bat
@@ -1,5 +1,5 @@
-# pybabel extract -F babel.cfg -k lazy_gettext -o lang/messages.pot .
-# pybabel init -i lang/messages.pot -d lang/translations -l ja
+#pybabel init -i lang/messages.pot -d lang/translations -l ja
 
+pybabel extract -F babel.cfg -k lazy_gettext -o lang/messages.pot .
 pybabel update -i lang/messages.pot -d lang/translations
 pybabel compile -d lang/translations
